880dd445d9803e78f4cae14a374bcdf3bc7d2036
Try <utkuozmelioglu@outlook.com>
2024-05-09 21:01:09 +0300

    First Try

---
+++ src/FileReader.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileReader {
+    String fileRoot = "Firma.txt";
+
+
+    public static void reader(String fileRoot){
+        Linked Customers=new Linked();
+        ArrayList<ArrayList<Integer>> PointList = new ArrayList<>();
+        try {
+            File file = new File(fileRoot);
+
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] firstLine=line.split(",");
+            int NumberOfProducts=Integer.parseInt(firstLine[0]);
+            List<String> NamesOfProducts=new ArrayList<>();
+            for (int i = 1; i < firstLine.length; i++){
+                NamesOfProducts.add(firstLine[i]);
+            }
+            line = scanner.nextLine();
+            String[] FirstCustomerLine=line.split(",");
+            Customers.addHead(Integer.parseInt(FirstCustomerLine[0]),new CustomerData(FirstCustomerLine[1],FirstCustomerLine[2],FirstCustomerLine[3],FirstCustomerLine[4],FirstCustomerLine[5]));
+            line = scanner.nextLine();
+            System.out.println(line);
+            String[] RatingLine=line.split(",");
+            ArrayList<Integer> FirstList = new ArrayList<>();
+            FirstList.add(Integer.parseInt(FirstCustomerLine[0]));
+            for (String rating:RatingLine){
+                FirstList.add(Integer.parseInt(rating));
+            }
+            PointList.add(FirstList);
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                String[] CustomerLine=line.split(",");
+                Customers.addLast(Integer.parseInt(CustomerLine[0]),new CustomerData(CustomerLine[1],CustomerLine[2],CustomerLine[3],CustomerLine[4],CustomerLine[5]));
+                line = scanner.nextLine();
+                RatingLine=line.split(",");
+                FirstList = new ArrayList<>();
+                FirstList.add(Integer.parseInt(CustomerLine[0]));
+                for (String rating:RatingLine){
+                    FirstList.add(Integer.parseInt(rating));
+                }
+                PointList.add(FirstList);
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Dosya bulunamadı: " + fileRoot);
+            e.printStackTrace();
+        }
+        Customers.Writer();
+        for (ArrayList rating:PointList){
+            System.out.println(rating);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -6,59 +6,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
-
-
     public static void main(String[] args) {
         String fileRoot = "Firma.txt";
         Linked Customers=new Linked();
         ArrayList<ArrayList<Integer>> PointList = new ArrayList<>();
-        try {
-            File file = new File(fileRoot);
-
-            Scanner scanner = new Scanner(file);
-            String line = scanner.nextLine();
-            String[] firstLine=line.split(",");
-            int NumberOfProducts=Integer.parseInt(firstLine[0]);
-            List<String> NamesOfProducts=new ArrayList<>();
-            for (int i = 1; i < firstLine.length; i++){
-                NamesOfProducts.add(firstLine[i]);
-            }
-            line = scanner.nextLine();
-            String[] FirstCustomerLine=line.split(",");
-            Customers.addHead(Integer.parseInt(FirstCustomerLine[0]),new CustomerData(FirstCustomerLine[1],FirstCustomerLine[2],FirstCustomerLine[3],FirstCustomerLine[4],FirstCustomerLine[5]));
-            line = scanner.nextLine();
-            System.out.println(line);
-            String[] RatingLine=line.split(",");
-            ArrayList<Integer> FirstList = new ArrayList<>();
-            FirstList.add(Integer.parseInt(FirstCustomerLine[0]));
-            for (String rating:RatingLine){
-                FirstList.add(Integer.parseInt(rating));
-            }
-            PointList.add(FirstList);
-            while (scanner.hasNextLine()) {
-                line = scanner.nextLine();
-                String[] CustomerLine=line.split(",");
-                Customers.addLast(Integer.parseInt(CustomerLine[0]),new CustomerData(CustomerLine[1],CustomerLine[2],CustomerLine[3],CustomerLine[4],CustomerLine[5]));
-                line = scanner.nextLine();
-                RatingLine=line.split(",");
-                FirstList = new ArrayList<>();
-                FirstList.add(Integer.parseInt(CustomerLine[0]));
-                for (String rating:RatingLine){
-                    FirstList.add(Integer.parseInt(rating));
-                }
-                PointList.add(FirstList);
-                System.out.println(line);
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Dosya bulunamadı: " + fileRoot);
-            e.printStackTrace();
-        }
-        Customers.Writer();
-        for (ArrayList rating:PointList){
-            System.out.println(rating);
-        }
-
+        FileReader.reader(fileRoot);
     }
 }
